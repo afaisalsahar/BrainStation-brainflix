a58637fe45b111e819a676afe01a2eefbccebf48
afaisalsahar <afaisalsahar@gmail.com>
2022-11-05 17:45:39 -0400

    Complete Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,30 @@
+import './Header.scss';
+
+import brainFlixLogo from '../../assets/logo/BrainFlix-logo.svg';
+
+import Avatar from '../Avatar/Avatar';
+import CallToAction from '../CallToAction/CallToAction';
+
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <header className="masthead">
+            <div className="masthead__container">
+                <a className="masthead__go-home" href="/">
+                    <img className="masthead__logo" src={brainFlixLogo} alt="BrainFlix Logo" />
+                </a>
+                <div className="masthead__actions">
+                    <form className="masthead__form">
+                        <input type="search" className="masthead__search" name="masthead--search" id="masthead--search" placeholder="Search" />
+                    </form>
+
+                    <Avatar showImage={true} defaultAvatar={true} avatarAlt="user avatar" />
+
+                    <form className="masthead__upload">
+                        <CallToAction modifier="upload" text="Upload"/>
+                    </form>
+                </div>
+            </div>
+        </header>
     );
 };
 
