fd9650c5c919cc5353231624558ab7b800d9a34b
afaisalsahar <afaisalsahar@gmail.com>
2022-11-05 18:38:00 -0400

    Complete video-player component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.scss';
-import Header from './components/Header/Header';
+import { useState } from 'react';
 
+import './App.scss';
+
+import Header from './components/Header/Header';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
+
+import videoDetails from './data/video-details.json';
+import videos from './data/videos.json';
 
 function App() {
+  
+  const [playingNow, setPlayingNow] = useState(
+    videoDetails[
+      Math.floor(
+        Math.random() * ((videos.length-1) - 0 + 1) + 0
+      )
+    ]
+  );
+
+
   return (
-    <Header />
+    <>
+      <Header />
+      <VideoPlayer poster={playingNow.image} />
+    </>
   );
 }
 
--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,7 +1,13 @@
-const VideoPlayer = () => {
+import './VideoPlayer.scss';
+
+const VideoPlayer = ({poster}) => {
     return (
-        <div>
-            
+        <div className="player__stage">
+            <div className="player__container">
+                <div className="player__video">
+                    <video className="player__media" poster={poster} controls></video>
+                </div>
+            </div>
         </div>
     );
 };
