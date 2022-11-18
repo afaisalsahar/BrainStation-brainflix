55907df86000f0922ad27b0c1966933aa89da388
afaisalsahar <afaisalsahar@gmail.com>
2022-11-17 21:55:50 -0500

    Integrate API GET requests

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,23 @@
 import './Home.scss'
 
-import { useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+// load hook dependencies 
+import { useEffect, useState } from 'react'; // perform side effects and preserve application state
+import { useParams, useNavigate } from 'react-router-dom'; // get params from URL PATH and redirection
 
+// load axios to make API calls
 import axios from 'axios';
 
+// load components required to render home page
 import Main from '../../components/Main/Main';
 import VideoPlayer from '../../components/VideoPlayer/VideoPlayer';
 import VideoList from '../../components/VideoList/VideoList';
 import Conversation from '../../components/Conversation/Conversation';
 
-const URL = 'https://project-2-api.herokuapp.com';
+// define API URL fields
+const URL = 'http://localhost';
+const PORT = '8080';
 const VID_ENDPOINT = 'videos';
-const API_KEY = 'bc3e6a76-4848-4adf-bdae-03dfc0597ac6';
+const API_KEY = 'removelater';
 
 const Home = () => {
     const [playingNow, setPlayingNow] = useState(null);
@@ -20,25 +25,23 @@ const Home = () => {
     const [videoList, setVideoList] = useState(null);
     
     const {id} = useParams();
-
     const navigate = useNavigate();
 
     useEffect(() => {
-        axios.get(`${URL}/${VID_ENDPOINT}?api_key=${API_KEY}`)
+        axios.get(`${URL}:${PORT}/${VID_ENDPOINT}`)
         .then(response => {
             setVideoList(
                 ((id && response.data.filter(video => video.id !== id)) || response.data.splice(1))
-            );
-
-            return axios.get(`${URL}/${VID_ENDPOINT}/${(id || response.data[0].id)}?api_key=${API_KEY}`)
-            .then(response => {
-                setPlayingNow(response.data);
-                setTopVidComments(response.data.comments)
-            })
+            )
             
-            .catch(error => {
-                navigate("/error404");
-            })
+            return axios.get(`${URL}:${PORT}/${VID_ENDPOINT}/${(id || response.data[0].id)}`);
+        })
+        .then(response => {
+            setPlayingNow(response.data);
+            setTopVidComments(response.data.comments)
+        })
+        .catch(error => {
+            navigate("/error404");
         })
     }, [id, navigate]);
     
